c6c05835f628a586f5c4714b4b4593353e60b686
Ivanfer1010 <ivgualotunabo@uide.edu.ec>
2026-03-23 12:30:29 +0000

    listprivatekeys

---
+++ script_gnupg.sh
@@ -0,0 +1,6 @@
+#Listar los claves privadas
+gpg --list-secret-keys --keyid-format=long
+#sec   rsa4096/35513F1E4D3380C9 2026-03-18 [SC] [expires: 2027-03-18]
+#      9A8C1BA9AADE1E5F261A6B8A35513F1E4D3380C9
+#uid                 [ultimate] Ivan Fernando Gualotuña Bolagay (12345678) <ivanfernando10q@gmail.com>
+#ssb   rsa4096/1A4B43578FF5785F 2026-03-18 [E] [expires: 2027-03-18]
