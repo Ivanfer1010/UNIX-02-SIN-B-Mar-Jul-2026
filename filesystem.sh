6a405d4f032008ab22931a1d1da25d13aee57ce5
Ivanfer1010 <ivgualotunabo@uide.edu.ec>
2026-03-25 13:45:34 +0000

    commandsexplanations_2

--- filesystem.sh
+++ filesystem.sh
@@ -2,3 +2,5 @@ pwd #Print working direction, muestra el directotrio en el que estamos trabajand
 cd / #change directory
 ls-F #Lista los archivos y muestra el contenido de la carpeta solicitada, agregando un símbolo al final de cada nombre que indica su tipo.
 ls -i #Muestra el número de inodo de cada archivo junto a su nombre.
+  echo "Hola" > test.txt #Imprimo el texto en la terminal y con > se redirecciona
+ stat test.txt  #Muestra infromacio detallada sobre el archivo test.txt
