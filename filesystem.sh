6b64467be2f018177e0380ca6f2e0422baa51c53
Ivanfer1010 <ivgualotunabo@uide.edu.ec>
2026-03-25 13:20:11 +0000

    explanation commands

--- filesystem.sh
+++ filesystem.sh
@@ -1,1 +1,4 @@
 pwd #Print working direction, muestra el directotrio en el que estamos trabajando.
+cd / #change directory
+ls-F #Lista los archivos y muestra el contenido de la carpeta solicitada, agregando un símbolo al final de cada nombre que indica su tipo.
+ls -i #Muestra el número de inodo de cada archivo junto a su nombre.
