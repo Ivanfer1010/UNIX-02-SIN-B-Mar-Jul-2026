a1ee360e9006ba16f2613058beb617270a9ae9da
Ivanfer1010 <ivgualotunabo@uide.edu.ec>
2026-03-30 12:54:15 +0000

    install_parted

---
+++ install_parted.sh
@@ -0,0 +1,7 @@
+#It keeps the system up to date, as it allows synchronizing the list of available packages with the official repositories.
+ sudo apt update
+#Download and install the latest versions of all software packages installed on theLinux system.
+ sudo apt upgrade
+#Install GNU Parted, a command-line tool for managing disk partitions in Linux.
+ sudo apt install parted
+
