4b91afe193d2f29a1226e84821057423e8b18e03
Ivanfer1010 <ivgualotunabo@uide.edu.ec>
2026-04-06 12:31:59 +0000

    commands_explanation1

---
+++ script_boot.sh
@@ -0,0 +1,8 @@
+#Change directory
+cd
+#Using cd move directly to the /home/codespace folder from wherever you are in the terminal.
+cd /home/codespace
+#It takes you directly to your personal directory.
+cd ~
+#Changes the directory to home
+cd $HOME
