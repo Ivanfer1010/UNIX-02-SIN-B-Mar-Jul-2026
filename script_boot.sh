a1c3c3eead9430cceda303cc0c4de1e7299ebdeb
Ivanfer1010 <ivgualotunabo@uide.edu.ec>
2026-04-06 12:51:07 +0000

    explanation_stat

--- script_boot.sh
+++ script_boot.sh
@@ -15,3 +15,5 @@ ls -lai
 #total 12
 #925547 drwxr-xr-x 2 codespace codespace 4096 Apr  6 12:37 .
 #918515 drwxr-x--- 1 codespace codespace 4096 Apr  6 12:37 ..
+#Displays detailed information about the current  directory like the birth of the file, acces, ,modify , inode, ect
+stat .
