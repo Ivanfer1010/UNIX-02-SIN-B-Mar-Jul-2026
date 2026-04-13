e3a2ece7c5ec70d4a742a6a7e129f9f22d0cbd82
Ivanfer1010 <ivgualotunabo@uide.edu.ec>
2026-04-13 12:49:16 +0000

    mkdir_rf_explanations

--- script.sh
+++ script.sh
@@ -10,3 +10,8 @@ ls -l -ah
 ls -lah
 #This three commands are practicaly the same but one is more shorter and this allow speed.
 #Lists files and directories in human-readable language
+mkdir -- -rf
+#Create a directory named rf
+rm -rf
+rm -- -rf
+#It is used to recursively and forcibly delete files and directories in this case rf.
