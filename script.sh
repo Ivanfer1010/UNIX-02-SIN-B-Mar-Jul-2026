7cd07d0f337507375e184b47a0f7c2e6b6ec116c
Ivanfer1010 <ivgualotunabo@uide.edu.ec>
2026-04-13 13:12:15 +0000

    chmod_explanations

--- script.sh
+++ script.sh
@@ -26,3 +26,11 @@ man git-clone
 --depth
 #Create a shallow clone with a history truncated to the specified number of commits. Implies --single-branch unless --no-single-branch is given to fetch the
 #histories near the tips of all branches. If you want to clone submodules shallowly, also pass --shallow-submodules.
+chmod +x script.sh
+#everyone can run it
+chmod u+x script.sh 
+#Only the owner can run it.
+chmod 0-r secreto.txt
+#Remove lecture to others
+chmod u+rw,go-rwx privado
+#Owner read/write, no one else can do anything.
