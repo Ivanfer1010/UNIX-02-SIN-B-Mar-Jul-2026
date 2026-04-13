09b6a901db9c7b0d9be7c48ce532937827fd4e7e
Ivanfer1010 <ivgualotunabo@uide.edu.ec>
2026-04-13 12:44:58 +0000

    ls -h explanations

--- script.sh
+++ script.sh
@@ -3,3 +3,10 @@ ls -a
 ls --all
 #It is the same than the other command but in larger version.
 #Command-Options-Arguments
+ls -h
+#This command is for human readable
+ls -l -a -h
+ls -l -ah
+ls -lah
+#This three commands are practicaly the same but one is more shorter and this allow speed.
+#Lists files and directories in human-readable language
