5621ca492b10448917cf60c9859305547643b402
Ivanfer1010 <ivgualotunabo@uide.edu.ec>
2026-04-15 13:05:48 +0000

    part_2_excercise

--- ejercicio1.sh
+++ ejercicio1.sh
@@ -1,2 +1,4 @@
 echo '#!bin/sh' > hola.sh
 #Create a file called hola.sh and write #!bin/sh
+echo 'echo "Hola desde mi primer script'" >> hola.sh
+#Add the line echo "Hola desde mi primer script" to the end of the hola.sh file.
