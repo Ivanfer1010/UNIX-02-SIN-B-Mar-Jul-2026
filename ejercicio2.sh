d0bf4deb13f6e315f0358e94b604c0f0546ff69c
Ivanfer1010 <ivgualotunabo@uide.edu.ec>
2026-04-15 13:39:34 +0000

    excercise_2

--- ejercicio2.sh
+++ ejercicio2.sh
@@ -1,2 +1,11 @@
 touch prueba.txt
 #Create a an empty file.
+chmod 600 prueba.txt
+#Change the permissions of test.txt so that only the owner can read and write it.
+ls -l prueba.txt
+#Displays detailed permissions and attributes of prueba.txt
+chmod 755 prueba.txt
+#Change the permissions
+ls -l prueba.txt
+#-rwxr-xr-x 1 codespace codespace 0 Apr 15 13:34 prueba.txt
+#Shows the changed permissions
