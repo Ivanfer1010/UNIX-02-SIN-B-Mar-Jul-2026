77e3a06671aeef8336d4d19d2293811d8d878723
Ivanfer1010 <ivgualotunabo@uide.edu.ec>
2026-04-15 13:09:32 +0000

    ./hola.sh

--- ejercicio1.sh
+++ ejercicio1.sh
@@ -2,3 +2,12 @@ echo '#!bin/sh' > hola.sh
 #Create a file called hola.sh and write #!bin/sh
 echo 'echo "Hola desde mi primer script'" >> hola.sh
 #Add the line echo "Hola desde mi primer script" to the end of the hola.sh file.
+cat hola.sh
+#$ cat hola.sh
+##!bin/sh
+#echo "Hola desde mi primer script"
+#Print the content of the file hola.sh
+./hola.sh
+#bash: ./hola.sh: Permission denied
+#Doesnt work becasuse we dont have enough permissions
+ls -l hola.sh
