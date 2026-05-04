65a36f71f4bf800ac1ae0e5db89931f9b48dde58
Ivanfer1010 <ivgualotunabo@uide.edu.ec>
2026-05-04 12:45:19 +0000

    id_cat_security.sh_1

---
+++ security.sh
@@ -0,0 +1,4 @@
+id
+#It is used to display information about the current user.
+cat /etc/passwd | head -10
+#It is used to display only the first 10 lines of the /etc/passwd file, which contains information about system users.
