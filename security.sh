561b46916be7265dc36aea589c476d1e31b485c5
Ivanfer1010 <ivgualotunabo@uide.edu.ec>
2026-05-04 13:35:12 +0000

    grep_root_id_explanation

--- security.sh
+++ security.sh
@@ -8,3 +8,13 @@ groups
 #Shows the groups to which the current user belongs in this case root
 groups $USER
 #Displays the groups to which the user whose name is stored in the $USER environment variable belongs
+id -u
+#Shows the User ID
+id -g
+#Shws the main group ID
+id -G 
+#Shoes all the ID groups
+cat /etc/group | grep root
+#Filter the /etc/group file to show only the information for the root group.
+cat /etc/gshadow
+#It displays the contents of the /etc/gshadow file, which stores secure information about  system groups, including group passwords.
