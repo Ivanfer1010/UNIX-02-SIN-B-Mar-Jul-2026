7b539203d051b7c917a4b114749351a5dce08253
Ivanfer1010 <ivgualotunabo@uide.edu.ec>
2026-06-03 13:34:10 +0000

    forced_error

--- blackhatbash5.sh
+++ blackhatbash5.sh
@@ -4,4 +4,20 @@ touch test && touch test123
 (ls; ps)
 ls; ps; whoami
 lzl || echo "el comando lzl fallo"
+echo "uno" > archivo.txt
+cat archivo.txt
+echo"uno-uno" > archivo.txt
+cat archivo.txt
+echo "dos-dos" >> archivo.txt
+cat archivo.txt
+echo "tres" &> archivo.txt
+cat archivo.txt
+echo "tres" &>> archivo.txt
+cat archivo.txt
+ls -l / &> stdout_and_stderr.txt
+cat stdout_and_stderr.txt
+ls -l / 1> stdout.txt 2> stderr.txt
+lzl 2> error.txt
+cat error.txt
+
 set +x
