a3d5df71b1095f761a01581033094f1fc3777cc0
Ivanfer1010 <ivgualotunabo@uide.edu.ec>
2026-06-17 12:43:14 +0000

    awk_print_NR

---
+++ blackhatbash8.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+awk '{print $1,$NF}' log.txt
+awk '{print $1}' log.txt
+awk '{print $2}' log.txt
+awk '{print $3}' log.txt
+awk '{print $1,$NF}' log.txt
+awk '{print $NF}' log.txt
+awk -F',' '{print $1}' example_csv.txt
+awk 'NR < 10' log.txt
+head log.txt
